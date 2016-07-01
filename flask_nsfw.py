1ff65fea2a1c34ecb64c8f7c5be68a6fe7953194
smit thakkar <smit@smits-MacBook-Air.local>
2016-07-01 09:00:49 -0400

    Finishing todos

--- flask_nsfw.py
+++ flask_nsfw.py
@@ -1,8 +1,10 @@
-from flask import current_app, make_response, request,abort
+from flask import current_app, make_response, request, abort, jsonify
 import os
 from functools import update_wrapper
 from clarifai.client import ClarifaiApi
 import util
+import tempfile
+
 
 try:
     from flask import _app_ctx_stack as stack
@@ -11,47 +13,51 @@ except:
 
 clarifai_api = ClarifaiApi(model='nsfw-v1.0')
 
-class flask_nsfw(object):
-    def __init__(self,app):
+class NSFW(object):
+    def __init__(self, app):
         self.app = app
         self.init_app(app)
 
-    def init_app(self,app):
+    def init_app(self, app):
         assert app.config['CLIENT_APP_ID'] is not None, 'CLIENT_APP_ID seems to be missing'
         assert app.config['CLIENT_APP_SECRET'] is not None, 'CLIENT_APP_SECRET seems to be missing'
         os.environ['CLARIFAI_APP_ID'] = app.config['CLIENT_APP_ID']
         os.environ['CLARIFAI_APP_SECRET'] = app.config['CLIENT_APP_SECRET']
 
     @classmethod
-    def block_nsfw(*args, **kwargs):
+    def block(*args, **kwargs):
         def decorator(f):
             def wrapped_function(*args, **kwargs):
+                import pdb; pdb.set_trace()
                 if request.method == 'POST' or request.method == 'PUT':
-                    if len(request.files.keys()) > 0:
-                        for key in request.files:
-                            result = clarifai_api.tag_images(request.files[key])
-                            if result['results'][0]['result']['tag']['classes'][0] == 'nsfw':
-                                result = result['results'][0]['result']['tag']['probs']
-                                if result[0] > 0.7:
-                                    abort(400)
-                    elif request.json is not None:
-                        for key in request.json:
-                            if util.is_base64(request.json[key]):
-                                base64String = request.json[key]
-                                file = open('temp.jpg','w+')
-                                file.write(base64String.decode('base64'))
-                                result = clarifai_api.tag_images(file)
-                                file.close()
-                            if result['results'][0]['result']['tag']['classes'][0] == 'nsfw':
-                                result = result['results'][0]['result']['tag']['probs']
-                                if result[0] > 0.7:
-                                    abort(400)
-                try:
-                    os.system('rm temp.jpg')
-                except:
-                    pass
-                # resp = current_app.make_default_options_response()
+                    result = True
+                    if request.files:
+                        result = NSFW.testFilesAgainstApi(request.files.values())
+                    elif request.json:
+                        result = NSFW.testBase64Data(request.json.values())
+                    if result == False:
+                        return jsonify({"response":"seems like the request contains the images that contains nudity."}), 403
                 resp = make_response(f(*args, **kwargs))
                 return resp
             return update_wrapper(wrapped_function, f)
         return decorator
+
+    @classmethod
+    def testFilesAgainstApi(self, data):
+        results = clarifai_api.tag_images(data)
+        for result in results['results']:
+            if result['result']['tag']['classes'][0] == 'nsfw':
+                result = result['result']['tag']['probs'][0]
+                if result > 0.7:
+                    return False
+        return True
+
+    @classmethod
+    def testBase64Data(self, data):
+        files = []
+        for d in data:
+            f = tempfile.TemporaryFile()
+            if util.is_base64(d):
+                f.write(d.decode('base64'))
+                files.append(f)
+        return self.testFilesAgainstApi(files)
--- util.py
+++ util.py
@@ -7,3 +7,20 @@ def is_base64(s):
         return enc == s
     except TypeError:
         return False
+
+from flask import jsonify
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        return rv
